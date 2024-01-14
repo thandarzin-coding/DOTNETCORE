f8f7d8a80f4d2b8c3a7e4eb2ddcd7c4d039e2b49
Thandar Zin <thandarzin@acedatasystems.com>
2024-01-14 10:48:23 +0630

    Add project files.

---
+++ DOTNETCORE.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNETCORE.ConsoleApp.AdoDotNetExamples
+{
+    public class AdoDotNetExample
+    {
+        public void Run()
+        {
+            //Read();
+            //Create("Test Title", "Test Author", "Test Content");
+            //Edit(1);
+            Update(1, "Testing Title", "Testing Author", "Testing Content");
+        }
+
+        public void Read()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+
+            };
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"SELECT [Blog_Id]
+      ,[Blog_Title]
+      ,[Blog_Author]
+      ,[Blog_Content]
+  FROM [dbo].[Tbl_Blog]";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Id =>" + dr["Blog_Id"]);
+                Console.WriteLine("Id =>" + dr["Blog_Author"]);
+                Console.WriteLine("Id =>" + dr["Blog_Title"]);
+                Console.WriteLine("Id =>" + dr["Blog_Content"]);
+            }
+
+        }
+
+        public void Edit(int id)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+
+            };
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"SELECT [Blog_Id]
+      ,[Blog_Title]
+      ,[Blog_Author]
+      ,[Blog_Content]
+  FROM [dbo].[Tbl_Blog] Where Blog_Id = @Blog_Id";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            command.Parameters.AddWithValue("Blog_Id", id);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No Data found");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine("Id =>" + dr["Blog_Id"]);
+            Console.WriteLine("Id =>" + dr["Blog_Author"]);
+            Console.WriteLine("Id =>" + dr["Blog_Title"]);
+            Console.WriteLine("Id =>" + dr["Blog_Content"]);
+            Console.WriteLine("........................");
+
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+
+            };
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [Blog_Title] = @Blog_Title
+      ,[Blog_Author] = @Blog_Author
+      ,[Blog_Content] = @Blog_Content
+ WHERE Blog_Id = @Blog_Id";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            command.Parameters.AddWithValue("@Blog_Id", id);
+            command.Parameters.AddWithValue("@Blog_Title", title);
+            command.Parameters.AddWithValue("@Blog_Author", author);
+            command.Parameters.AddWithValue("@Blog_Content", content);
+
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+            if (result == 0)
+            {
+                Console.WriteLine("No Data found");
+                return;
+            }
+
+            string message = result > 0 ? "Updated Successful." : "Updating Failed.";
+            Console.WriteLine(message);
+        }
+        public void Create(string title, string author, string content)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sa@123",
+                TrustServerCertificate = true
+
+            };
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([Blog_Title]
+           ,[Blog_Author]
+           ,[Blog_Content])
+     VALUES
+           ('Blog_Title', 
+           'Blog_Author'
+           ,'Blog_Content')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            command.Parameters.AddWithValue("@Blog_Title", title);
+            command.Parameters.AddWithValue("@Blog_Author", author);
+            command.Parameters.AddWithValue("@Blog_Content", content);
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ DOTNETCORE.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DOTNETCORE.ConsoleApp.AdoDotNetExamples;
+using System.Data.SqlClient;
+using System.Data;
+Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Run();
+
+Console.ReadKey();
